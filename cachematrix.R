627f80f5e73bba99cb2cba773a390597bab31339
IsabelClara <isa_lage@hotmail.com>
2016-12-09 20:11:17 +0100

    ass2 correction

--- cachematrix.R
+++ cachematrix.R
@@ -24,7 +24,7 @@ cacheSolve <- function(x, ...) {
   inv <- x$getinv()
   if(!is.null(inv)) {
     message("getting cached data")
-    return(m)
+    return(inv)
   }
   data <- x$get()
   m <- solve(data, ...)
